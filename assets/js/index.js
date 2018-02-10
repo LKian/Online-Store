69003455ec90b1e40cc8790b7a0741d38c5233f2
Kahwasaurus <leahkahwadjian@gmail.com>
2018-02-09 20:48:36 -0500

    first commit:  navbar

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+console.log("TASKS");
+console.log("1.  Replace right menu with social media icons");
+console.log("2.  Create toggle bar for tablet/mobile window");
+console.log("3.  Create store items for sale.  Visitor should have option to make the items block view or grid view.");
+
